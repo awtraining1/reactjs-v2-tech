bd9da29c4482954f360d4fe80ab4aa5a18ff9b62
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2022-08-10 18:45:32 +0530

    react js starter project

---
+++ Day3/myreactproject/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+          Welcome to V2
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React in a Great Way starting today
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
